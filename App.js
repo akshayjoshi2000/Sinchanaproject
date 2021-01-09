c0a66e46e91f186662d633d980b6b921716b9714
akshayjoshi2000 <39109833+akshayjoshi2000@users.noreply.github.com>
2021-01-09 23:27:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './Header.js'
+import Boc from './Boc.js'
+
+function App() {
+  return (
+    <div className="app">
+      {/* <LoginPage/> */}
+      <Header/>
+      <Boc/>
+      {/* <ItemPage/> */}
+     
+      {/* <Cart/> */}
+      {/* <Checkout/> */}
+
+      
+    </div>
+  );
+}
+
+export default App;
