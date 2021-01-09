970501029a92282ebb8514b2acb4deeef80f972f
akshayjoshi <joshiakshay000@gmail.com>
2021-01-09 23:49:55 +0530

    merge after project creation

---
+++ crm/src/Boc.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Boc.css';
+import ItemCard from './ItemCard.js'
+
+function Boc() {
+    return (
+        <div className='body'>
+          <h1>
+            yoo body here 
+             <ItemCard/>
+         </h1>  
+        </div>
+    )
+}
+
+export default Boc
